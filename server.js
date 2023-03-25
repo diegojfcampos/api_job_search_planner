70da934a11cfe58cd14dea1b395dec390951aaa5
diegojfcampos <diegojfcampos@gmail.com>
2023-03-25 23:42:12 +0000

    config: plugins fasltify @mongodb @env @session @cookie @auth @jwt  and bcryptjs

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', async (request, reply) =>{
     reply.send({Status: "SERVER RUNNING"})
 })
 
+app.post('/login', async (request, reply) =>{
+    reply.send({Status: "LOGIN"})
+})
+
+
 //Setting Up server
 const start = async () => {
     try{
