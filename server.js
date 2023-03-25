a0286f38d9e0affccd5d2774009e6a1bbb854a40
diegojfcampos <diegojfcampos@gmail.com>
2023-03-25 19:27:32 +0000

    config: NodeJS, FastifyJS and plugins cors, swagger and uuid

---
+++ server.js
@@ -0,0 +1,25 @@
+const app = require('fastify')({ logger: true })
+const cors = require('@fastify/cors')
+
+//Setting Up Cors
+app.register(cors,{ 
+    origin: true,
+    methods: 'GET,POST,PUT,DELETE'
+})
+
+//Seting Up Routes
+app.get('/', async (request, reply) =>{
+    reply.send({Status: "SERVER RUNNING"})
+})
+
+//Setting Up server
+const start = async () => {
+    try{
+        await app.listen({port: 3000})        
+    }catch(error){
+        app.log.error(error)
+        process.exit(1)
+    }
+}
+
+start();
