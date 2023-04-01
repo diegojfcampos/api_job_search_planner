79e3f6e88401dde4ab4636b7b2aecdbe8b3ffe87
diegojfcampos <diegojfcampos@gmail.com>
2023-04-01 09:09:18 +0100

    refactor: register validations

--- src/models/registerSchema.js
+++ src/models/registerSchema.js
@@ -5,11 +5,36 @@ const registerSchema = {
         _id: {type: 'string'},
         username: {type: 'string', format: 'email',  pattern: '[-!#$%&\'*+\\/0-9=?A-Z^_a-z{|}~](\\.?[-!#$%&\'*+\\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\\.?[a-zA-Z0-9])*\\.[a-zA-Z](-?[a-zA-Z0-9])+'},
         password: {type: 'string'},
-        job_tracker:{type: 'array', default: [], items: {type: 'object'}},
-        recruiter_tracker:{type: 'array', default: [], items: {type: 'object'}},
-        
+        job_tracker: {
+            type: 'array', 
+            items: {
+                type: 'object', 
+                properties: {
+                    _id: {type: 'string'},
+                    job_title: {type: 'string'},
+                    company: {type: 'string'},
+                    date_applied: {type: 'string'},
+                    status: {type: 'string'},
+                    notes: {type: 'string'},     
+                    date_added: {type: 'string'},
+                    date_updated: {type: 'string'},
+                },
+            },
+        },
+        recruiter_tracker: {
+            type: 'array', 
+            items: {
+                type: 'object', 
+                properties: {
+                    _id: {type: 'string'},
+                    recruiter_name: {type: 'string'},
+                    recruiter_email: {type: 'string'},
+                    recruiter_phone: {type: 'string'},
+                    recruiter_notes: {type: 'string'},
+                },
+            },    
+        },   
     }
-}
-
+};
 
 module.exports = registerSchema;
--- src/routes/register.js
+++ src/routes/register.js
@@ -8,20 +8,27 @@ async function registerRoute(app, options, done) {
     app.post('/register', {schema: {body: registerSchema}}, async (request, reply) => {
 
         const {username, password} = request.body;
-
+    
         const db = app.mongo.db;
         const collection = db.collection('users');
         
         const user = {
             _id: uuidv4(),
             username,
-            password: await bcrypt.hash(password, 256),
+            password: await bcrypt.hash(password, 12),
             job_tracker: [],
             recruiter_tracker: [],
-        };        
-
+        };
+    
+        const isValid = app.validator.validate(registerSchema, request.body);
+    
+        if (!isValid) {
+            reply.status(400).send({ error: 'Invalid request data' });
+            return;
+        }
+    
         const resultPostUser = await collection.insertOne(user);
-
+    
         if (resultPostUser.result.n === 1) {
             const token = app.jwt.sign({username: user.username, id: user._id}, app.config.SECRET, { expiresIn: '2h' });
             reply.send({ token });
@@ -29,6 +36,7 @@ async function registerRoute(app, options, done) {
             reply.status(500).send({ error: 'Failed to create user' });
           }
     }),
+    
     
     done()
 }
